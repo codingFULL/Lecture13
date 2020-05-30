5fdead541772e126be92c9ad350d5295e7c18686
Andres <acja87@hotmail.com>
2020-05-30 11:52:40 -0500

    se agregan estilos

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,12 +1,13 @@
+import './home.css';
+
 import React from 'react';
 
 import { Link } from 'react-router-dom';
 
 function Home(props) {
-  console.log(props);
   return (
     <div>
-      <h1>{'Hola desde la página de inicio :)'}</h1>
+      <h1>{'Bienvenid@ a nuestra aplicación'}</h1>
       <ol>
         <li>
           <Link to="/signIn">{'Página de ingreso'}</Link>
