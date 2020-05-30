066074bd1d77cb75b467f368e5168da068f1b269
Andres <acja87@hotmail.com>
2020-05-30 12:06:46 -0500

    se agregan imagenes

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,10 +4,24 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
+import logo from '../../assets/images/logo.png';
+
 function Home(props) {
   return (
     <div>
       <h1>{'Bienvenid@ a nuestra aplicación'}</h1>
+      <div>
+        <img src={logo} alt="ejemplo logo" className="imagen1" />
+      </div>
+
+      <div>
+        <img
+          src="https://www.coca-cola.com.co/content/dam/brands/lc/coca-cola/images/Logotipo-CocaCola-Colombia.png"
+          alt="ejemplo logo"
+          className="imagen1"
+        />
+      </div>
+
       <ol>
         <li>
           <Link to="/signIn">{'Página de ingreso'}</Link>
